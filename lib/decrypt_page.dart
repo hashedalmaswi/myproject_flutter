984d9fedc8f55000e8bd1937d48dd63e97667ee7
hashed <hashed@gmail.com>
2025-09-26 23:23:14 +0300

    إضافة صفحة “المساعدة” HelpPage مع شرح الاستخدام

--- lib/decrypt_page.dart
+++ lib/decrypt_page.dart
@@ -120,7 +120,7 @@ class _DecryptPageState extends State<DecryptPage> {
                 TextField(
                   controller: _customKey,
                   decoration: const InputDecoration(
-                    labelText: 'مفتاحالصص',
+                    labelText: 'مفتالصص',
                     border: OutlineInputBorder(),
                     prefixIcon: Icon(Icons.vpn_key),
                     helperText: 'لا رفًا',
