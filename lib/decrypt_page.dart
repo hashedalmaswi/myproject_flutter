80465b4334dfce89390b295004b207cda86ce4d4
hashed <hashed@gmail.com>
2025-09-26 22:48:25 +0300

    إضافة أيقونات مميزة لكل خوارزمية

--- lib/decrypt_page.dart
+++ lib/decrypt_page.dart
@@ -76,7 +76,7 @@ class _DecryptPageState extends State<DecryptPage> {
     return DropdownButtonFormField<String>(
       value: _algorithm,
       decoration: const InputDecoration(
-        labelText: 'ر الخوارزمية',
+        labelText: 'ر لخوارزمية',
         border: OutlineInputBorder(),
         prefixIcon: Icon(Icons.security),
       ),
