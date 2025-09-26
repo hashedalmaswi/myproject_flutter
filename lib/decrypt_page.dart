97bf402597c353cd5ce3a1d9507f039db2811ea5
hashed <hashed@gmail.com>
2025-09-26 22:49:16 +0300

    إإضافة Drawer (قائمة جانبية للتنقل)

--- lib/decrypt_page.dart
+++ lib/decrypt_page.dart
@@ -76,7 +76,7 @@ class _DecryptPageState extends State<DecryptPage> {
     return DropdownButtonFormField<String>(
       value: _algorithm,
       decoration: const InputDecoration(
-        labelText: 'ر لخوارزمية',
+        labelText: 'لخوارزمية',
         border: OutlineInputBorder(),
         prefixIcon: Icon(Icons.security),
       ),
