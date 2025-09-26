113d63c65ed0101881e36d625a9ec94f22038188
hashed <hashed@gmail.com>
2025-09-26 22:45:02 +0300

    تخصيص Theme عام للتطبيق

--- lib/decrypt_page.dart
+++ lib/decrypt_page.dart
@@ -76,7 +76,7 @@ class _DecryptPageState extends State<DecryptPage> {
     return DropdownButtonFormField<String>(
       value: _algorithm,
       decoration: const InputDecoration(
-        labelText: 'اتر الخوارزمية',
+        labelText: 'ر الخوارزمية',
         border: OutlineInputBorder(),
         prefixIcon: Icon(Icons.security),
       ),
