11aa2497ce96c6d598a87d855b3a098f58e182e8
hashed <hashed@gmail.com>
2025-09-26 23:19:34 +0300

    إعادة تنظيم الملفات داخل lib/

--- lib/decrypt_page.dart
+++ lib/decrypt_page.dart
@@ -120,7 +120,7 @@ class _DecryptPageState extends State<DecryptPage> {
                 TextField(
                   controller: _customKey,
                   decoration: const InputDecoration(
-                    labelText: 'مفتاح AES الصص',
+                    labelText: 'مفتاحES الصص',
                     border: OutlineInputBorder(),
                     prefixIcon: Icon(Icons.vpn_key),
                     helperText: 'ل تا 3حرفًا',
