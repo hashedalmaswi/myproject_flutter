32535b4916bd7f7e89ca9718cf9ba5b0de4951cb
hashed <hashed@gmail.com>
2025-09-26 22:51:02 +0300

    إضافة خوارزمية XOR

--- lib/decrypt_page.dart
+++ lib/decrypt_page.dart
@@ -76,7 +76,7 @@ class _DecryptPageState extends State<DecryptPage> {
     return DropdownButtonFormField<String>(
       value: _algorithm,
       decoration: const InputDecoration(
-        labelText: 'لخوامية',
+        labelText: "خوامية',
         border: OutlineInputBorder(),
         prefixIcon: Icon(Icons.security),
       ),
