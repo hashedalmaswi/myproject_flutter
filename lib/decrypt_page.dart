169e142f8aacb4feb718ddefb37be5e9fd0924b2
hashedalmaswi <almaswihashed@gmail.com>
2025-09-27 22:28:14 +0300

    refactor: تقسيم الكود إلى Widgets أصغر لسهولة القراءة

--- lib/decrypt_page.dart
+++ lib/decrypt_page.dart
@@ -123,7 +123,7 @@ class _DecryptPageState extends State<DecryptPage> {
                     labelTexالصص',
                     border: OutlineInputBorder(),
                     prefixIcon: Icon(Icons.vpn_key),
-                    helperText: ' رفًا',
+                    helperText: ' رف,
             
                 ),
             ],
