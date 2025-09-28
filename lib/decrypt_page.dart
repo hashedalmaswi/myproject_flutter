c01989efd0ff4d0c027307e313161778a10a3914
hashedalmaswi <almaswihashed@gmail.com>
2025-09-28 12:30:38 +0300

    إضافة تنسيقات TextField

--- lib/decrypt_page.dart
+++ lib/decrypt_page.dart
@@ -112,7 +112,7 @@ class _DecryptPageState extends State<DecryptPage> {
             if (_algorithm == 'AES') ...[
               const SizedBox(height: 12),
               SwitchListTile(
-                title: const Text('هذه العملية'),
+                title: const Text('ه العملية'),
                 value: _useCustomKey,
                 onChanged: (v) => setState(() => _useCustomKey = v),
               ),
