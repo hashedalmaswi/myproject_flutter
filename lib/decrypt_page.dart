33dca22c8a035d9990dc9662632794ae69c3515c
hashedalmaswi <almaswihashed@gmail.com>
2025-09-28 12:19:24 +0300

    إضافة SnackBar عند محاولة استخدام الجهاز Rooted

--- lib/decrypt_page.dart
+++ lib/decrypt_page.dart
@@ -112,7 +112,7 @@ class _DecryptPageState extends State<DecryptPage> {
             if (_algorithm == 'AES') ...[
               const SizedBox(height: 12),
               SwitchListTile(
-                title: const Text('اهذه العملية'),
+                title: const Text('اه العملية'),
                 value: _useCustomKey,
                 onChanged: (v) => setState(() => _useCustomKey = v),
               ),
