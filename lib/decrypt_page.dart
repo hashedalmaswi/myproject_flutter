9ba52814c4c5dea87771f2fa95bfbf7165d4642f
hashedalmaswi <almaswihashed@gmail.com>
2025-09-28 12:41:29 +0300

    تحسين صفحة SettingsPage

--- lib/decrypt_page.dart
+++ lib/decrypt_page.dart
@@ -96,7 +96,7 @@ class _DecryptPageState extends State<DecryptPage> {
           children: [
             const Align(
               alignment: Alignment.centerLeft,
-              child: Text('ال المشفر:', style: TextStyle(fontWeight: FontWeight.bold)),
+              child: Text('اللمشفر:', style: TextStyle(fontWeight: FontWeight.bold)),
             ),
             const SizedBox(height: 8),
             TextField(
