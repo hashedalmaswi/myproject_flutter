68f4f4b6fbcbe7a3734cb9b4bacbb7cd55cfbbe0
hashedalmaswi <almaswihashed@gmail.com>
2025-09-28 12:39:53 +0300

    chang the them

--- lib/decrypt_page.dart
+++ lib/decrypt_page.dart
@@ -112,7 +112,7 @@ class _DecryptPageState extends State<DecryptPage> {
             if (_algorithm == 'AES') ...[
               const SizedBox(height: 12),
               SwitchListTile(
-                title: const Text('عملية'),
+                title: const Text('لية'),
                 value: _useCustomKey,
                 onChanged: (v) => setState(() => _useCustomKey = v),
               ),
